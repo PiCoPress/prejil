aa4ee9b9768b20364c48ba2eacc89c30ce99bd80
PICOPress <choys041011@gmail.com>
2024-03-24 21:54:51 +0900

    Figure out

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,16 +1,27 @@
 mod rslib;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    use rslib::base::*;
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let mut t = Base::init("araabaaq");
+        let res = t.seek_char('b');
+        assert_eq!(match res {
+            Some(ct) => ct.value,
+            None => vec!('s')
+        }, vec!['r', 'a', 'a']);
+    }
+
+    #[test]
+    fn copy_paste_char_vec() {
+        let input = b"dsd";
+        print!("[");
+        for i in input[..input.len() - 2].iter() {
+            print!("'{}', ", *i as char);
+        }
+        println!("'{}']", input[input.len() - 1] as char);
+        assert_eq!(2, 1);
     }
 }
--- engine/src/rslib/base.rs
+++ engine/src/rslib/base.rs
@@ -1,30 +1,44 @@
-struct Base {
-    cursor: u32,
-    flag: i8,
-    data: str,
-    value: u16,
-    length: u32,
-}
 impl Base {
-    fn seek_char(&mut self, c: &char) {
-        for to_find in c[self.cursor..] {
-            if c == to_find {
-                self.flag = 1;
-                return;
+    pub fn init(dat: &'static str) -> Base {
+        Base {
+            cursor: 0,
+            data: dat,
+        }
+    }
+    pub fn seek_char(&mut self, c: char) -> Option<Found> {
+        let mut bind: Vec<char> = Vec::new();
+        let c: u8 = c as u8;
+        let ii = self.cursor + 1;
+        for to_find in self.data[ii..].as_bytes().iter() {
+            if &c == to_find {
+                return Some(Found {value: bind});
             }
+            bind.push(char::from(*to_find));
             self.cursor += 1;
         }
-        std::process::exit(2);
+        self.cursor = ii - 1;
+        return None;
     }
-    fn seek_back(&mut self, c: &char) {
-        let c = vec!(c).iter().rev();
-        for to_find in c[..self.cursor] {
-            if c == to_find {
-                self.flag = 1;
-                return;
+    pub fn seek_char_back(&mut self, c: char) -> Option<Found> {
+        let mut ii = self.cursor;
+        let c: u8 = c as u8;
+        let mut bind: Vec<char> = Vec::new();
+        let self_copy = self.data.as_bytes();
+        while ii > 0 {
+            if self.data.as_bytes()[ii - 1] == c {
+                self.cursor = ii;
+                return Some(Found {value: bind});
             }
-            self.cursor += 1;
+            bind.push(self_copy[ii - 1] as char);
+            ii -= 1;
         }
-        std::process::exit(2);
+        return None;
     }
+}
+pub struct Base {
+    cursor: usize,
+    data: &'static str,
+}
+pub struct Found {
+    pub(crate) value: Vec<char>,
 }
